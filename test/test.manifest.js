04bf5f068f1a020f5094118dd812f8caa8306215
Jennifer Fong <jen@ednapiranha.com>
2014-06-19 17:40:12 -0700

    remove file check

--- test/test.manifest.js
+++ test/test.manifest.js
@@ -5,53 +5,24 @@ process.env.NODE_ENV = 'test';
 var should = require('should');
 var Manifest = require('../src/manifest');
 var m = new Manifest();
+var content = '';
 
-describe('setFiles', function () {
-  it('should return an array of files in the packaged app path', function (done) {
-    m.appPath = './test/apps/app2';
-
-    m.setFiles(function (err, result) {
-      should.exist(result);
-      result.should.eql(['./test/apps/app2/manifest.webapp']);
+describe('validate', function () {
+  it('should return an invalid manifest error', function (done) {
+    m.validate(content, function (err, result) {
+      should.exist(err);
+      err.toString().should.equal('Error: Manifest is not in a valid JSON format');
       done();
     });
   });
-});
 
-describe('validateManifest', function () {
-  it('should return an missing manifest error', function (done) {
-    m.appPath = './test/apps/app1';
+  it('should return an valid manifest object', function (done) {
+    content = '{}';
 
-    m.setFiles(function (err, result) {
-      m.validateManifest(function (err, result) {
-        should.exist(err);
-        err.toString().should.equal('Error: No manifest.webapp file found');
-        done();
-      });
-    });
-  });
-
-  it('should return an invalid manifest error', function (done) {
-    m.appPath = './test/apps/app2';
-
-    m.setFiles(function (err, result) {
-      m.validateManifest(function (err, result) {
-        should.exist(err);
-        err.toString().should.equal('Error: Manifest is not in a valid JSON format');
-        done();
-      });
-    });
-  });
-
-  it('should return an valid manifest file object', function (done) {
-    m.appPath = './test/apps/app3';
-
-    m.setFiles(function (err, result) {
-      m.validateManifest(function (err, result) {
-        should.exist(result);
-        result.should.eql({});
-        done();
-      });
+    m.validate(content, function (err, result) {
+      should.exist(result);
+      result.should.eql({});
+      done();
     });
   });
 });
